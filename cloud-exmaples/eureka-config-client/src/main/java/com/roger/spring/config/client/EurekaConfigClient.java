5f08d023dbd4f0e6f7c2ed6d72530516bc88783a
chenluojie <chen.rojer@gmail.com>
2016-07-08 09:37:52 +0800

    添加eureka config server 的context-path，实现config server 的URL 可变。添加配套的config client 配置说明

--- cloud-exmaples/eureka-config-client/src/main/java/com/roger/spring/config/client/EurekaConfigClient.java
+++ cloud-exmaples/eureka-config-client/src/main/java/com/roger/spring/config/client/EurekaConfigClient.java
@@ -3,12 +3,15 @@ package com.roger.spring.config.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 /**
  * Created by chenluojie on 2016/7/7.
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+//此注解在远程配置管理数据改变后，通过调用/refresh rest 接口，可以刷新数据值
+@RefreshScope
 public class EurekaConfigClient {
 
     public static void main(String args[]){
--- cloud-exmaples/eureka-config-client/src/main/java/com/roger/spring/config/client/controller/TestController.java
+++ cloud-exmaples/eureka-config-client/src/main/java/com/roger/spring/config/client/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.roger.spring.config.client.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by chenluojie on 2016/7/7.
  */
 @RestController
+//此注解在远程配置管理数据改变后，通过调用/refresh rest 接口，可以刷新数据值
+@RefreshScope
 public class TestController {
     @Value("${hellow}")
     private String hellow;
